636aff4cbb11a0b67b029603a7cb7a7fd6052c3d
root <agroce@gmail.com>
2019-04-23 06:44:59 +0000

    deepstate version

--- vuln.cpp
+++ vuln.cpp
@@ -8,11 +8,11 @@ int vulnfunc(int32_t intInput, char * strInput) {
    if (2 * intInput + 1 == 31337)
       if (strcmp(strInput, "Bad!") == 0)
          assert(0);
+   return 0;
 }
 
 TEST(FromEclipser, CrashIt) {
-   char buf[9] = (char*)DeepState_Malloc(9);
+   char *buf = (char*)DeepState_Malloc(9);
    buf[8] = 0;
    vulnfunc(*((int32_t*) &buf[0]), &buf[4]);
-   return 0;
 }
